dc254ef52d8f81d345f7cea7fdd780bad3b6497d
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-07-15 15:35:19 +0200

    integration

---
+++ Front/Examen_Final/pages/detail_rendez-vous.php
@@ -0,0 +1,40 @@
+<h3>Mes rendez-vous</h3>
+<hr>
+<table class="table table-striped">
+    <tr>
+        <th>Matricule</th>
+        <td>4323 TBE</td>
+    </tr>
+    <tr>
+        <th>Type Voiture</th>
+        <td>Legere</td>
+    </tr>
+    <tr>
+        <th>Slot</th>
+        <td>A</td>
+    </tr>
+    <tr>
+        <th>Type reparation</th>
+        <td>Vidange</td>
+    </tr>
+    <tr>
+        <th>Duree</th>
+        <td>1 h</td>
+    </tr>
+    <tr>
+        <th>Date Rendz-vous</th>
+        <td>12-07-2024</td>
+    </tr>
+    <tr>
+        <th>Cout</th>
+        <td>15 000 Ar</td>
+    </tr>
+    <tr>
+        <th>Date payement</th>
+        <td>12-07-2024</td>
+    </tr>
+</table>
+<hr>
+<form action="pdf.php">
+    <input type="submit" class="bouton_2" value="Exporter en PDF">
+</form>
