dc44804ef579ab351109d84979cee9b037b6c741
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2024-07-16 03:04:10 +0300

    initial free slot function

--- application/models/Suggestion_model.php
+++ application/models/Suggestion_model.php
@@ -1,10 +1,23 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-final class Suggestion_model extends CI_Model
-{	
-	public function get_dispo(){
-		return $this->db->
+class Suggestion_model extends CI_Model
+{
+	public function get_free_slots($required_diff)
+	{
+		$sql = "
+            SELECT *
+            FROM finals4_view_free_slots
+            WHERE diff > ?";
+
+		$query = $this->db->query($sql, array($required_diff));
+		return $query->result();
 	}
+
+	public function get_free_slots_for_service($service_id){
+		$sql = "";
+		$query = $this->db->query($sql, array($service_id));
+	}
+
 }
