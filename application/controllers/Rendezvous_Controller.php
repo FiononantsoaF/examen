6374b8b836bb847d3da5176e64605ae524d77c78
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2024-07-16 09:31:27 +0300

    tena vita le izy

--- application/controllers/Rendezvous_Controller.php
+++ application/controllers/Rendezvous_Controller.php
@@ -1,33 +1,38 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Rendezvous_Controller extends CI_Controller {
+class Rendezvous_Controller extends CI_Controller
+{
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('Rendezvous_model');  // Assurez-vous de charger le modèle ici
-        $this->load->library('session');
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Rendezvous_model');  // Assurez-vous de charger le modèle ici
+		$this->load->library('session');
+	}
 
-    public function index() {
-        $this->load->view('templates/header.php');
-        $this->load->view('rendezvous/rendezvous.php');
-        $this->load->view('templates/footer.php');
-    }
+	public function index()
+	{		
+		$this->load->view('templates/header.php');
+		$this->load->view('rendezvous/rendezvous.php');
+		$this->load->view('templates/footer.php');
+	}
 
-    public function liste_rendezvous() {
-        $this->load->view('templates/header.php');
-        $client_id = $this->session->userdata('client_id');
-        $data['list_rendezvous'] = $this->Rendezvous_model->get_rendezvousbyclient($client_id);
-        $this->load->view('rendezvous/liste_rendezvous.php',$data);
-        $this->load->view('templates/footer.php');
-    }
+	public function liste_rendezvous()
+	{
+		$this->load->view('templates/header.php');
+		$client_id = $this->session->userdata('client_id');
+		$data['list_rendezvous'] = $this->Rendezvous_model->get_rendezvousbyclient($client_id);
+		$this->load->view('rendezvous/liste_rendezvous.php', $data);
+		$this->load->view('templates/footer.php');
+	}
 
-    public function detail_rendezvous($idr){
-        $this->load->view('templates/header.php');
-        $data['detail_rendezvous'] = $this->Rendezvous_model->get_detail_rendezvous($idr);
-        $this->load->view('rendezvous/detail_rendezvous.php',$data);
-        $this->load->view('templates/footer.php');
-    }
+	public function detail_rendezvous($idr)
+	{
+		$this->load->view('templates/header.php');
+		$data['detail_rendezvous'] = $this->Rendezvous_model->get_detail_rendezvous($idr);
+		$this->load->view('rendezvous/detail_rendezvous.php', $data);
+		$this->load->view('templates/footer.php');
+	}
 
 }
